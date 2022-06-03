6864aab4a7529810b3d3cf6d2de97750ea4be09e
EJreal <ejreales75@gmail.com>
2022-06-03 05:39:19 -0500

    React-dom fixed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import store from './store';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>, document.getElementById('root'));
+  </Provider>);
 
